ad1bb13ad505c4a40b0e4940a8d37d0ac3ab5455
Albert Abdulmanov <albert.abdu@gmail.com>
2021-08-08 15:03:35 +0300

    add some links to the header

--- src/components/header/links.ts
+++ src/components/header/links.ts
@@ -1,9 +1,9 @@
 import { Link } from '../../types/Link'
 
 export const links: Link[] = [
-  {
-    href: '/',
-    text: 'Home',
-  },
+  { href: '/', text: 'Home' },
   { href: '/wishlist', text: 'Wishlist' },
+  { href: 'https://blog.lbrt.me', text: 'Blog ↗' },
+  { href: 'https://minecraft.lbrt.me', text: 'Minecraft server ↗' },
+  { href: 'https://demo.lbrt.me', text: 'Demos ↗' },
 ]
