9f23a31312a83fd8bbd58addd92f4f04a00711b5
Albert <albert.abdu@gmail.com>
2022-12-04 20:43:22 +0100

    Update links.ts

--- src/components/header/links.ts
+++ src/components/header/links.ts
@@ -3,7 +3,7 @@ import { Link } from '../../types/Link'
 export const links: Link[] = [
   { href: '/', text: 'Home' },
   { href: '/wishlist', text: 'Wishlist' },
-  { href: 'https://blog.kitcat.one', text: 'Blog ↗' },
-  { href: 'https://minecraft.kitcat.one', text: 'Minecraft server ↗' },
-  { href: 'https://demo.kitcat.one', text: 'Demos ↗' },
+  { href: 'https://blog.kitcat.dev', text: 'Blog ↗' },
+  { href: 'https://minecraft.kitcat.dev', text: 'Minecraft server ↗' },
+  { href: 'https://demo.kitcat.dev', text: 'Demos ↗' },
 ]
