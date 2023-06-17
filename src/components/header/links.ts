56cf5f778dd6480878546d4eddc4267d0201ead4
Albert Abdulmanov <26366132+kitcat-dev@users.noreply.github.com>
2023-06-17 21:08:57 +0200

    remove link to minecraft server from header

--- src/components/header/links.ts
+++ src/components/header/links.ts
@@ -4,6 +4,5 @@ export const links: Link[] = [
   { href: '/', text: 'Home' },
   { href: '/wishlist', text: 'Wishlist' },
   { href: 'https://blog.kitcat.dev', text: 'Blog ↗' },
-  { href: 'https://minecraft.kitcat.dev', text: 'Minecraft server ↗' },
   { href: 'https://demo.kitcat.dev', text: 'Demos ↗' },
 ]
